79be712d0c1e09294061c1d5005a86c27c5d2955
fc-anjos <felipe.anjos@usp.br>
2020-07-03 13:44:31 -0300

    implement react

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Leads list</h1>
+      </div>
+    );
+  }
+}
+
+export default Form
