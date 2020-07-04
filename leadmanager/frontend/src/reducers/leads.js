2ab9162090a6faa15dfaf6ec271ff7d9ae517c8a
fc-anjos <felipe.anjos@usp.br>
2020-07-04 11:35:50 -0300

    add delete lead button, not yet functioning

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -15,7 +15,7 @@ export default function(state = initialState, action) {
     case DELETE_LEAD:
       return {
         ...state,
-        leads: state.leads.filter(lead => lead.id !== action.payload),
+        leads: state.leads.filter(lead => lead.id != action.payload),
       };
     default:
       return state;
